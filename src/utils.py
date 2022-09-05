e9876cfa83b8a0e1e799e03d34a88de320f08bfb
zanderbraam <zander@nmrql.com>
2022-09-05 16:54:31 +0200

    Added function to read and error check datasets

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,28 @@
-import os
+from pathlib import Path
+from dateutil.parser import parse
+
+
+def get_project_root():
+    """
+    Return the project root path
+
+    :return: string path
+    """
+    return Path(__file__).parent.parent
+
+
+def is_date(string, fuzzy=False):
+    """
+    Return whether the string can be interpreted as a date.
+
+    :param string: str, string to check for date
+    :param fuzzy: bool, ignore unknown tokens in string if True
+    """
+    try:
+        parse(string, fuzzy=fuzzy)
+        return True
+
+    except ValueError:
+        return False
 
 
